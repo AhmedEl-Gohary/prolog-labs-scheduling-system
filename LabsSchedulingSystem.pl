4565036e1c6ebccece59ae6d2029aaf146fc1a5d
AhmedEl-Gohary <ahmedelgohary.stem@gmail.com>
2023-04-15 16:33:14 +0200

    fix typo and spacing

--- LabsSchedulingSystem.pl
+++ LabsSchedulingSystem.pl
@@ -1,5 +1,5 @@
 % Predicate (e):
-% Updates RemTAs list after assining the TA with name 'Name'
+% Updates RemTAs list after assigning the TA with name 'Name'
 
 ta_slot_assignment([], [], _).
 
@@ -7,6 +7,6 @@ ta_slot_assignment(TAs, RemTAs, Name) :-
 	TAs = [ta(TAName, Load)|T],
 	ta_slot_assignment(T, PreviousRemTAs, Name),
 	((TAName = Name, Load > 0, NewLoad is Load - 1);
-	 (TAName \= Name, NewLoad is Load)),
+	(TAName \= Name, NewLoad is Load)),
 	RemTAs = [ta(TAName, NewLoad)|PreviousRemTAs].
 
