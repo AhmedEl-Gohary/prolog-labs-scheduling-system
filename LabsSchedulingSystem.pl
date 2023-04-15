e0e19085a80655c6755570db4a5c459fe266e83c
AhmedEl-Gohary <ahmedelgohary.stem@gmail.com>
2023-04-15 16:30:31 +0200

    add ta_slot_assignment predicate

--- LabsSchedulingSystem.pl
+++ LabsSchedulingSystem.pl
@@ -1,2 +1,12 @@
-main :-
-	write('Hello World').
+% Predicate (e):
+% Updates RemTAs list after assining the TA with name 'Name'
+
+ta_slot_assignment([], [], _).
+
+ta_slot_assignment(TAs, RemTAs, Name) :-
+	TAs = [ta(TAName, Load)|T],
+	ta_slot_assignment(T, PreviousRemTAs, Name),
+	((TAName = Name, Load > 0, NewLoad is Load - 1);
+	 (TAName \= Name, NewLoad is Load)),
+	RemTAs = [ta(TAName, NewLoad)|PreviousRemTAs].
+
