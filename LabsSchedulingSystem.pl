b30eb1d1b3ff39da240804d45a4447ec3404aca7
AhmedEl-Gohary <ahmedelgohary.stem@gmail.com>
2023-04-15 21:31:44 +0200

    add slot_assignement predicate

--- LabsSchedulingSystem.pl
+++ LabsSchedulingSystem.pl
@@ -1,5 +1,26 @@
+% Predicate(d):
+% Generates all the possible TA assignments for labs running during the same slot 
+% TODO: check if it is possible to use ta_slot_assignment instead of repeating code
+% TODO: handling non-positive Load
+% TODO: check if there is a better way than generating all perumtations
+
+slot_assignment(LabsNum, TAs, RemTAs, Assignment) :-
+	permutation(TAs, NewPermutation),
+	helper_slot_assignment(LabsNum, NewPermutation, RemTAs, Assignment).
+
+helper_slot_assignment(0, TAs, TAs, []). 
+
+helper_slot_assignment(LabsNum, TAs, RemTAs, Assignment) :-
+	TAs = [ta(Name, Load)|T],
+	NewLabsNum is LabsNum - 1, 
+	helper_slot_assignment(NewLabsNum, T, PreviousRemTAs, PreviousAssignment),
+	NewLoad is Load - 1,
+	RemTAs = [ta(Name, NewLoad)|PreviousRemTAs],
+	Assignment = [Name|PreviousAssignment].
+	
 % Predicate (e):
-% Updates RemTAs list after assigning the TA with name 'Name'
+% Updates TAs list into RemTAs after assigning the TA with name 'Name' a new slot
+% The load of the chosen TA is decremented
 
 ta_slot_assignment([], [], _).
 
